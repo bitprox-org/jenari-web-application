141a336f92e140aa845f49e56d046c4bc9bba6f4
kassy <ichokusomtoo13@gmail.com>
2024-12-04 04:36:43 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"dm-sans"', "sans-serif"],
+        nunito: ['"nunito-sans"', "sans-serif"],
+      },
+      colors: {
+        "header-bg": "#1A1A1A",
+        "primary-bg": "#0D8C42",
+        "secondary-bg": "#DA7656",
+      },
+    },
+  },
+  plugins: [],
+};
