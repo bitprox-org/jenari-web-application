6316c6352a58c9deb1f4ddaf70477ff44354d769
kassy <ichokusomtoo13@gmail.com>
2024-12-04 06:01:49 +0100

    kassy index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "./layouts/Layout"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Index from "./pages/Index";
 
 function App() {
-
-
   return (
-  <BrowserRouter>
-  <Routes>
-
-    <Route path="/" element={<Layout/>}> 
-
-    </Route>
-  </Routes>
-  
-  </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Index />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Categories from "../components/Categories";
+
+const Index = () => {
+  return (
+    <div>
+      <Hero />
+      <Categories />
+    </div>
+  );
+};
+
+export default Index;
